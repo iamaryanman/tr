ae67977f117c69d756c01945e90bc707e4fcc7a1
iamaryanman <aryan5566.ji@gmail.com>
2025-02-14 17:55:28 +0530

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,62 @@
+import os
+import time
+import asyncio
+import logging
+from datetime import datetime
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+async def upload_video(client, file_path, thumbnail_path, video_title, reply_msg, collection_channel_id, user_mention, user_id, message):
+    file_size = os.path.getsize(file_path)
+    uploaded = 0
+    start_time = datetime.now()
+    last_update_time = time.time()
+
+    async def progress(current, total):
+        nonlocal uploaded, last_update_time
+        uploaded = current
+        percentage = (current / total) * 100
+        elapsed_time_seconds = (datetime.now() - start_time).total_seconds()
+        
+        if time.time() - last_update_time > 2:
+            progress_text = format_progress_bar(
+                filename=video_title,
+                percentage=percentage,
+                done=current,
+                total_size=total,
+                status=f"Uploading {'🪙' * (percentage // 10)}{'🧲' if percentage == 100 else ''}",
+                eta=(total - current) / (current / elapsed_time_seconds) if current > 0 else 0,
+                speed=current / elapsed_time_seconds if current > 0 else 0,
+                elapsed=elapsed_time_seconds,
+                user_mention=user_mention,
+                user_id=user_id,
+                aria2p_gid=""
+            )
+            try:
+                await reply_msg.edit_text(progress_text)
+                last_update_time = time.time()
+            except Exception as e:
+                logging.warning(f"Error updating progress message: {e}")
+
+    with open(file_path, 'rb') as file:
+        collection_message = await client.send_video(
+            chat_id=collection_channel_id,
+            video=file,
+            caption=f"✨ {video_title}\n👤 ʟᴇᴇᴄʜᴇᴅ ʙʏ : {user_mention}\n📥 ᴜsᴇʀ ʟɪɴᴋ: tg://user?id={user_id}",
+            thumb=thumbnail_path,
+            progress=progress
+        )
+        await client.copy_message(
+            chat_id=message.chat.id,
+            from_chat_id=collection_channel_id,
+            message_id=collection_message.id
+        )
+        await asyncio.sleep(1)
+        await message.delete()
+
+    await reply_msg.delete()
+    sticker_message = await message.reply_sticker("CAACAgUAAxkBAAEuTMFnBqKQ59KI3X1xdxzWsWcdqube3QACwhAAAmJHOFSiDwzU3LbwJDYE")
+    os.remove(file_path)
+    os.remove(thumbnail_path)
+    await asyncio.sleep(5)
+    await sticker_message.delete()
+    return collection_message.id
